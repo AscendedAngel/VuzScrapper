71a4978021f98fce6d1b477e0ab18ced5b03de2b
AscendedAngel <hikashimlbb@gmail.com>
2026-07-15 15:08:34 +0700

    (fix) Added targeted quota for ITMO (01.03.02)

--- Scrappers/Itmo/ItmoParser.cs
+++ Scrappers/Itmo/ItmoParser.cs
@@ -30,7 +30,7 @@ internal sealed partial class ItmoParser
 
         var subcompetitions = fullData.QuerySelectorAll(":scope > div[class^=RatingPage_table__]").ToArray();
 
-        var offset = programName.StartsWith("01.03.02") ? 0 : 1;
+        var offset = programName.StartsWith("!01.03.02") ? 0 : 1;
         var olympiads = subcompetitions[0];
         var targets = subcompetitions[offset];
         var specials = subcompetitions[1 + offset];
